dfa5cf459607fe62a36987d59153c839e297bb59
Jennifer W <jwharto1@gmail.com>
2021-01-02 13:22:02 -0500

    Added edit functionality.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -30,7 +30,6 @@ personRegistration.addEventListener('submit', async e => {
 
 function showPerson(data) {
     const personTable = document.getElementById("personTable");
-    let tableHeaders = ["ID", "Full Name"];
 
     data.forEach(person => {
         appendPersons(personTable, person.id, person.name)
@@ -49,9 +48,13 @@ const appendPersons = (personTable, id, name) => {
     personName.className = "name"
     personName.innerText = name;
 
+    let buttonGroup = document.createElement("td");
+    buttonGroup.className = "bttnGroup";
     const deleteButton = createDeleteButton();
+    const editButton = createEditButton();
+    buttonGroup.append(editButton, deleteButton);
 
-    personTableBodyRow.append(personID, personName, deleteButton);
+    personTableBodyRow.append(personID, personName, buttonGroup);
     personTable.append(personTableBodyRow);
 }
 
@@ -73,10 +76,31 @@ const createDeleteButton = () => {
     return deleteButton;
 }
 
+const createEditButton = () => {
+
+    const editButton = document.createElement("input");
+    editButton.type = "image";
+    editButton.src = "images/edit-button.png";
+    editButton.name = "editPerson";
+    editButton.className = "editPersonButton";
+
+    const bootstrapModalAttr = document.createAttribute("data-bs-toggle");
+    bootstrapModalAttr.value = "modal";
+    editButton.setAttributeNode(bootstrapModalAttr);
+
+    const bootstrapTargetAttr = document.createAttribute("data-bs-target");
+    bootstrapTargetAttr.value = "#exampleModal";
+    editButton.setAttributeNode(bootstrapTargetAttr);
+
+    editButton.onclick = editPerson;
+
+    return editButton;
+}
+
 function deletePerson() {
 
-    this.parentNode.remove();
-    const idToDelete = this.parentNode.getElementsByClassName("id").item(0).innerText;
+    this.parentNode.parentElement.remove();
+    const idToDelete = this.parentNode.parentElement.getElementsByClassName("id").item(0).innerText;
 
     const url = "http://localhost:8080/api/v1/person/" + idToDelete;
 
@@ -96,6 +120,58 @@ function deletePerson() {
         .then(data => console.log(data))
         .catch(err => console.log(err))
 }
+
+function createEditModalHeader(rowData) {
+    const editModalHeader = document.getElementById("exampleModalLabel");
+    editModalHeader.innerText = `Edit ${rowData.getElementsByClassName("name").item(0).innerText}`;
+}
+
+function editPerson() {
+
+    createEditModalHeader(this.parentNode.parentElement);
+
+    const idToEdit = this.parentNode.parentElement.getElementsByClassName("id").item(0).innerText;
+
+    const url = "http://localhost:8080/api/v1/person/" + idToEdit;
+
+    let personEditForm = document.getElementById('personEditForm');
+
+    submitHandler(personEditForm, url);
+
+}
+
+function submitHandler(personEditForm, url){
+    personEditForm.addEventListener('submit', async e => {
+
+        e.preventDefault();
+
+        let myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        let formData = new FormData(e.target);
+        let formDataEntries = Object.fromEntries(formData.entries());
+
+        let raw = JSON.stringify(formDataEntries);
+
+        let requestOptions = {
+            method: 'PUT',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch(url, requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+
+        window.location.reload();
+    });
+}
+
+
+
+
 
 
 
